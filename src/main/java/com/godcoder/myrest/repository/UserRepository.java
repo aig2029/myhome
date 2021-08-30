4068bda7acc938ffbe0c4208b2c8fb9b9f232f84
donaldlee <aig2029@naver.com>
2021-08-30 10:33:32 +0900

    Spring Boot으로 웹 출시까지 #..

---
+++ src/main/java/com/godcoder/myrest/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.godcoder.myrest.repository;
+
+import com.godcoder.myrest.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = { "boards" })
+    List<User> findAll();
+
+    User findByUsername(String username);
+}
